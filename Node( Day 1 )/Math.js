20d98ad742c0dc6d6798c94f150165d9a63767a4
Mihir Gupta <mihirkumargupta2@gmail.com>
2023-11-15 16:00:04 +0530

    Working with Node jS

---
+++ Node( Day 1 )/Math.js
@@ -0,0 +1,24 @@
+// Define functions for arithmetic operations
+const Sum = (a, b) => console.log(a + b);
+const Sub = (a, b) => console.log(a - b);
+const Multi = (a, b) => console.log(a * b);
+
+// Define a constant
+const PI = 3.14;
+
+// Create an object to organize and export the functions and constant
+const Calculator = {
+  Sum: Sum,
+  Sub: Sub,
+  Multi: Multi,
+  PI: PI
+};
+
+// Alternatively, you can use shorthand property names since the property name and the variable name are the same
+// const Calculator = { Sum, Sub, Multi, PI };
+
+// Export the object
+module.exports = Calculator;
+exports.number=123;
+// Additionally, you can export a variable directly using `exports`
+
---
+++ Node( Day 1 )/Script.js
@@ -0,0 +1,12 @@
+const Math = require("./Math"); // Corrected module name
+
+//console.log(process.argv);
+//console.log(calculator.Sub(10, 2)); // Using the correct method name
+//console.log(Math.number); // Accessing the exported variable
+
+console.log(Math.number);
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.Sub(20,10));
+
