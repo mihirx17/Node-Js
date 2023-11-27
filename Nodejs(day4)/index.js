b89b03264c0af5f74fc0e6f7e662d34e2b93e711
Mihir Gupta <mihirkumargupta2@gmail.com>
2023-11-27 22:34:42 +0530

    Added Edit.ejs

--- Nodejs(day4)/index.js
+++ Nodejs(day4)/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
 
 // Parse URL-encoded bodies (usually sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
+
+// Use method-override middleware
+app.use(methodOverride('_method'));
 
 // Set the views directory
 app.set("views", path.join(__dirname, "views"));
@@ -37,29 +41,57 @@ app.post("/post", (req, res) => {
     const newPost = { id: uuidv4(), username, postContent };
     posts.push(newPost);
 
-    // Assuming you want to redirect to the home page after processing the form
+    // Redirect to the home page after processing the form
     res.redirect('/');
 });
 
 app.get("/post/:id", (req, res) => {
     const id = req.params.id;
     const post = posts.find((p) => id == p.id);
-    console.log(post);
+    
+    if (!post) {
+        // Handle the case where the post is not found
+        res.status(404).send("Post not found");
+        return;
+    }
+
     res.render("Show.ejs", { post: post });
 });
+
 app.patch("/post/:id", (req, res) => {
     let id = req.params.id;
-    let newcontent=req.body;
-    console.log(newcontent);
-    console.log(id);
-    const post = posts.find((p) => id == p.id);
-    post.postContent=newcontent;
-    console.log(post);
+    let newContent = req.body.postContent; // Extract the specific property
 
-    res.send("Patch has been completed");
+    const post = posts.find((p) => id == p.id);
+
+    if (!post) {
+        // Handle the case where the post is not found
+        res.status(404).send("Post not found");
+        return;
+    }
+
+    post.postContent = newContent; // Update the specific property
+
+    console.log(post);
+  
+    res.redirect("/");
+});
+
+
+app.get("/post/:id/Edit", (req, res) => {
+    let id = req.params.id;
+    const post = posts.find((p) => id == p.id);
+
+    if (!post) {
+        // Handle the case where the post is not found
+        res.status(404).send("Post not found");
+        return;
+    }
+
+    res.render("Edit.ejs", { id, post });
+   
 });
 
 app.listen(8000, () => {
     console.log(`Connecting to port 8000`);
 });
- 
